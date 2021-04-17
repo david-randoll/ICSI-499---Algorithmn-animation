ec9be3657d10229d31ebd16bb2e6eb7c76f38ce8
Kyler <kyler.randall98@gmail.com>
2021-04-16 22:36:52 -0400

    Updating Settings MVC

--- Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Controllers/SettingsController.java
+++ Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Controllers/SettingsController.java
@@ -1,4 +1,26 @@
 package Controllers;
 
+import Models.SettingsModel;
+import Views.SettingsView;
+
 public class SettingsController {
+    private SettingsView settingsView;
+    private SettingsModel settingsModel;
+
+    public SettingsController(SettingsView settingsView, SettingsModel settingsModel) {
+        this.settingsView = settingsView;
+        this.settingsModel = settingsModel;
+        InitView();
+        InitController();
+    }
+
+
+    private void InitView(){
+
+    }
+
+    private void InitController(){
+
+    }
+
 }
--- Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Views/SettingsView.java
+++ Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Views/SettingsView.java
@@ -1,4 +1,9 @@
 package Views;
 
-public class SettingsView {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class SettingsView extends JPanel {
+
+    private JButton submit = new JButton("Submit");
 }
