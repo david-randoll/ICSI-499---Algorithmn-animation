350a1d6b1633dbe55b56587e85a8514581bb1cde
David Randoll <david.randoll@scubeenterprise.com>
2021-05-01 16:11:08 -0400

    Revert "change the home page code"
    This reverts commit 98333195a44d7e8476dda880bf578ecf8e20b15d.

--- Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Controllers/SettingsController.java
+++ Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Controllers/SettingsController.java
@@ -1,20 +1,25 @@
 package Controllers;
 
 import Models.SettingsModel;
+import Shared.DataAccess;
+import SharedComponents.DefaultFrame;
 import Views.SettingsView;
 import res.Styles;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SettingsController implements ActionListener {
     public SettingsView view;
-    public JFrame frame;
     private SettingsModel model = new SettingsModel();
+    private DefaultFrame frame;
+    private Color color;
 
-
-    public SettingsController(JFrame frame) {
+    public SettingsController(DefaultFrame frame) {
         this.frame = frame;
         InitView();
         InitController();
@@ -23,19 +28,35 @@ public class SettingsController implements ActionListener {
 
     private void InitView() {
         view = new SettingsView();
+        this.frame.add(this.view);
+        this.frame.setVisible(true);
+        this.frame.repaint();
     }
 
     private void InitController() {
 
         this.view.submit.addActionListener(this);
         this.view.Home.addActionListener(this);
+        this.view.titleColorsButton.addActionListener(this);
+
+        view.titleColors.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setColor();
+            }
+        });
+        view.feedback.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                view.feedback.setText("");
+            }
+        });
     }
 
     public void actionPerformed(ActionEvent e) {
         //Feedback form submit button
         if (e.getSource() == this.view.submit) {
-            System.out.println("Testing settings button");
-            String feedback = this.view.feedbackForm.getText();
+            //String feedback = this.view.feedbackForm.getText();
 
             //Connecting to Java spring Rest API
             //send data to db
@@ -44,12 +65,23 @@ public class SettingsController implements ActionListener {
         } else if (e.getSource() == this.view.fontColorSubmit) { //fontColor Color
 
         } else if (e.getSource() == this.view.Home) {
-            System.out.println("TEST");
-            HomeController homeController = new HomeController();
-            this.frame.getContentPane().removeAll();
-            this.frame.getContentPane().add(homeController.homeView);
-            this.frame.setVisible(true);
+            frame.getContentPane().removeAll();
+            this.frame.repaint();
+            HomeController homeController = new HomeController(this.frame);
         }
+        else if (e.getSource() == this.view.titleColors){
+            this.color=JColorChooser.showDialog(this.frame,"Select a color", Color.BLUE);
+            this.view.titleColors.setText(color.toString());
+        }
+        else if (e.getSource() == this.view.titleColorsButton){
+            Styles.PAGE_TITLE_FOREGROUNGCOLOR = color;
+            JOptionPane.showMessageDialog(null, "Return Home to view the changes");
+        }
+    }
+
+    public void setColor(){
+        this.color=JColorChooser.showDialog(this.frame,"Select a color", Color.BLUE);
+        this.view.titleColors.setText(color.toString());
     }
 
 }
