9c40cb23c1406ffc192510d4089e16fad01e4803
David Randoll <david.randoll@scubeenterprise.com>
2021-05-04 01:49:42 -0400

    remove unused code and fix bug

--- Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Models/SortingAlgorithms/QuickSortModel.java
+++ Algorithm Visualizer Solution/AlgorithmVisualizer.Swing/src/Models/SortingAlgorithms/QuickSortModel.java
@@ -9,12 +9,12 @@ import java.util.Arrays;
 public class QuickSortModel extends SorterModel implements IGeneratePanel {
 
     String TITLE = "Quick Sort";
-    ArrayList<Integer> sortedIndexList = new ArrayList<>();
+    ArrayList<Integer> sortedIndexList;
 
     public QuickSortModel() {
         super();
         int[] data = DataAccess.GetData();
-        ArrayList<Panel> panels = runClone(data);
+        ArrayList<Panel> panels = run(data);
 
         for (int i = 0; i < panels.size(); i++) {
             super.Panels.add(panels.get(i), Integer.toString(i));
@@ -23,57 +23,7 @@ public class QuickSortModel extends SorterModel implements IGeneratePanel {
 
     public ArrayList<Panel> run(int arr[]) {
 
-        ArrayList<Panel> output = new ArrayList<>();
-
-        int l = 0, r = arr.length - 1;
-
-        Panel firstPanel = new Panel(TITLE, arr, null, "");
-        output.add(firstPanel);
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                Integer[] indices = {j, j + 1};
-
-                ArrayList<Integer> list = new ArrayList<>();
-
-                for (int index = arr.length - i ; index < arr.length; index++) {
-                    list.add(index);
-                }
-                Integer[] sortedIndices = new Integer[list.size()];
-                sortedIndices = list.toArray(sortedIndices);
-
-                Panel newPanel = new Panel(TITLE, arr, sortedIndices , indices, "Is "+arr[j]+" greater than "+arr[j+1]+"?");
-                output.add(newPanel);
-
-
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    Integer[] swapIndices = {j + 1, j};
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    Panel swapPanel = new Panel(TITLE, arr, sortedIndices, swapIndices, "Yes, so we swap!");
-                    output.add(swapPanel);
-                }else {
-                    Panel noSwapPanel = new Panel(TITLE, arr, sortedIndices , indices, "Nope");
-                    output.add(noSwapPanel);
-                }
-            }
-        }
-
-        Integer[] sortedIndices = new Integer[arr.length];
-        for(int i =0; i < sortedIndices.length;i++){
-            sortedIndices[i] = i;
-        }
-
-        Panel lastPanel = new Panel(TITLE, arr, sortedIndices,null, "Sorted");
-        output.add(lastPanel);
-
-        return output;
-    }
-
-    public ArrayList<Panel> runClone(int arr[]) {
-
+        sortedIndexList = new ArrayList<>();
         ArrayList<Panel> output = new ArrayList<>();
 
         int l = 0, r = arr.length - 1;
